29c985684c6ed9e87712c0f3d115ee87905891d5
zzz077 <122636696+zzz-077@users.noreply.github.com>
2023-06-14 23:55:00 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -16,18 +16,23 @@ const closed_dropdown_bar_h1 = document.querySelectorAll(
 const coursesNav_boxULLI = document.querySelectorAll(".coursesNav_box ul li");
 const courses_cards = document.querySelectorAll(".courses_image_box");
 const counters = document.querySelectorAll(".counter");
-const categoryBox = document.querySelectorAll(".category-box");
-const categoryInputContainer = document.querySelectorAll(".category-input");
-const beneficiaries = document.getElementById("beneficiaries");
-const categories = document.getElementById("categories");
-const locations = document.getElementById("locations");
-const mainContainerEl = document.querySelector(".main-container");
+const contact_corner_box = document.querySelector(".contact_corner_box");
+const contact_corner_box_i = document.querySelector(".chat_open_btn");
+const contact_header_remove_btn = document.querySelector(
+  ".contact_header .contact_corner_box"
+);
 
-let startIndex = 0;
-let endIndex = 30;
-/*=====================  import data.js  ===========================*/
-import data from "./data.js";
+const message_box_remove_btn = document.querySelector(
+  ".message_box_remove_btn"
+);
+const contact_corner_content = document.querySelector(
+  ".contact_corner_content"
+);
 
+const chat_open_btn = document.querySelector(".chat_open_btn");
+const contact_input = document.querySelector(".contact_input_box input");
+const send_msg_btn = document.querySelector(".send_msg_btn");
+const contact_chat_box = document.querySelector(".contact_chat_box");
 /*===========================FUNCTIONS-indexDB=============================*/
 function scrollFunc() {
   navbarCont.classList.toggle("active", scrollY > 80);
@@ -36,7 +41,6 @@ function scrollFunc() {
     navbar_box2ULLI[i].classList.toggle("active", scrollY > 80);
   }
   navbar_search_Input.classList.toggle("active", scrollY > 80);
-  mainContainerEl.classList.toggle("active", scrollY > 80);
 }
 
 function NavsearchbaruClick() {
@@ -101,179 +105,80 @@ for (let i = 0; i < counters.length; i++) {
   staticTimer();
 }
 
+//online contact btn
+function contactBtnclickOpen() {
+  contact_corner_box.classList.add("active");
+  chat_open_btn.classList.add("active");
+  contact_corner_content.classList.remove("active");
+}
+
+function contactBtnclickClose() {
+  contact_corner_box.classList.remove("active");
+  chat_open_btn.classList.remove("active");
+  contact_corner_content.classList.add("active");
+}
+
+function scrollToBottom() {
+  contact_chat_box.scrollTop = contact_chat_box.scrollHeight;
+}
+
+function messageclickBtn() {
+  let day = new Date();
+  let hr = day.getHours();
+  let mn = day.getMinutes();
+  let sc = day.getSeconds();
+  if (contact_input.value.trim() != 0) {
+    let newmessage = document.createElement("div");
+    newmessage.classList.add("message_box");
+    newmessage.innerHTML = `
+        <h3>${hr + ":" + mn}</h3>
+        <div>
+          <p>${contact_input.value}</p>
+        </div>
+`;
+    contact_chat_box.appendChild(newmessage);
+    scrollToBottom();
+    contact_input.value = "";
+  }
+}
+
+function messageKeybindBtn(e) {
+  if (e.key == "Enter") messageclickBtn();
+}
+
 switch (Page) {
   case "indexBD":
     /*===========================EVENTLISTENERS=============================*/
     navbar_menu.addEventListener("click", navbarMenuClick);
     navbar_search_I.addEventListener("click", NavsearchbaruClick);
+    contact_corner_box_i.addEventListener("click", contactBtnclickOpen);
+    message_box_remove_btn.addEventListener("click", contactBtnclickClose);
+    send_msg_btn.addEventListener("click", messageclickBtn);
+    contact_input.addEventListener("keyup", messageKeybindBtn);
     window.addEventListener("scroll", scrollFunc);
 
     /*===========================sliders=============================*/
+    let slideIndex = 0;
+    showSlides();
 
-    var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      speed: 2000,
-      opacity: 0,
-      autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-      },
-    });
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i].style.opacity = "0";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      slides[slideIndex - 1].style.display = "block";
+      slides[slideIndex - 1].style.opacity = "1";
+      setTimeout(showSlides, 5000);
+    }
+
     break;
 
   case "servicesBD":
-    renderServices();
-
-    /*=========================== Variables After rendering =================*/
-
-    /*===========================EVENTLISTENERS=============================*/
-    navbar_menu.addEventListener("click", navbarMenuClick);
-    navbar_search_I.addEventListener("click", NavsearchbaruClick);
-    window.addEventListener("scroll", scrollFunc);
-    categoryBox[0].addEventListener("click", beneficiariesClick);
-    categoryBox[1].addEventListener("click", categoryClicks);
-    categoryBox[2].addEventListener("click", locationsClicks);
-    window.addEventListener("scroll", renderScroll);
-    document.addEventListener("click", (e) => {
-      if (e.target.dataset.titlecontainer) {
-        dropDownFunction(e.target.dataset.titlecontainer);
-      }
-    });
-    document.addEventListener("DOMContentLoaded", filterFunction);
     break;
-}
-
-/*============================Functions for "servicesBD" ======*/
-
-function beneficiariesClick() {
-  beneficiaries.classList.toggle("display");
-  document.querySelector(".benef").classList.toggle("rotate");
-  containerHeight(beneficiaries);
-}
-function categoryClicks() {
-  categories.classList.toggle("display");
-  document.querySelector(".category").classList.toggle("rotate");
-  containerHeight(categories);
-}
-function locationsClicks() {
-  locations.classList.toggle("display");
-  document.querySelector(".locations").classList.toggle("rotate");
-  containerHeight(locations);
-}
-
-function containerHeight(categoriesContainer) {
-  let containerHeight = 0;
-  for (let i = 0; i < categoryInputContainer.length; i++) {
-    containerHeight += categoryInputContainer[i].offsetHeight;
-  }
-  if (containerHeight > 300) {
-    console.log(containerHeight);
-    categoriesContainer.classList.remove("overflowScrollBar");
-  } else {
-    console.log(containerHeight);
-    categoriesContainer.classList.add("overflowScrollBar");
-  }
-}
-
-function dropDownFunction(titleId) {
-  const titleObj = data.filter(function (title) {
-    return title.id == titleId;
-  })[0];
-  console.log(titleObj);
-  document.getElementById(`${titleId}`).classList.toggle("display");
-  document.getElementById(`icon-${titleId}`).classList.toggle("rotatePlusIcon");
-}
-
-function feedServiceTabs() {
-  let maxIndex = startIndex + (endIndex - startIndex);
-  if (data.length < maxIndex) {
-    maxIndex = data.length;
-  }
-  for (let i = startIndex; i < maxIndex; i++) {
-    console.log("MaxIndex", maxIndex);
-    if (data.length >= maxIndex) {
-      let dataInfo = data[i];
-      let containerInfoDiv = document.createElement("div");
-      containerInfoDiv.classList.add("container-info");
-      containerInfoDiv.innerHTML = `<div class="title" data-titlecontainer="${dataInfo.id}">
-                                            <div class="main-info" data-titlecontainer="${dataInfo.id}">
-                                            <span data-titlecontainer="${dataInfo.id}">${dataInfo.title}</span>
-                                            <span class="more-info" data-titlecontainer="${dataInfo.id}"><div data-titlecontainer="${dataInfo.id}"><span class="span" data-titlecontainer="${dataInfo.id}">კატეგორია:</span> ${dataInfo.categories}</div><div data-titlecontainer="${dataInfo.id}"> <span class="span" data-titlecontainer="${dataInfo.id}">ლოკაცია:</span> ${dataInfo.location}</span></div>
-                                            </div>
-                                            <i class="fa-solid fa-circle-plus plus-icon " id="icon-${dataInfo.id}" data-titlecontainer="${dataInfo.id}"></i>
-                                          </div>
-                                          <div class="description" id="${dataInfo.id}" data-titlecontainer="${dataInfo.id}">
-                                            <p>
-                                              ${dataInfo.description}
-                                            </p>
-                                            <br />
-                                            <a
-                                              class="seeMorelink"
-                                              target="_blank"
-                                              href="${dataInfo.link}"
-                                              alt="დეტალურად ნახვა"
-                                              >დეტალურად</a>
-                                         </div>`;
-      document.querySelector(".render-here").append(containerInfoDiv);
-    }
-  }
-  startIndex = endIndex;
-  endIndex += endIndex + 30;
-}
-
-function renderServices() {
-  feedServiceTabs();
-}
-
-function renderScroll() {
-  let innerContainerHeight = 0;
-  let innerContainerEl = document.querySelectorAll(".container-info");
-  for (let i = 0; i < innerContainerEl.length; i++) {
-    innerContainerHeight += innerContainerEl[i].offsetHeight;
-  }
-  const scrollPosition = window.scrollY || window.pageYOffset;
-  const scrollThreshold = innerContainerHeight - window.innerHeight - 100;
-  console.log(innerContainerHeight);
-  if (scrollPosition > scrollThreshold) {
-    renderServices();
-  }
-}
-
-function filterFunction() {
-  const checkboxes = document.querySelectorAll(
-    '#categories input[type="checkbox"]'
-  );
-  console.log(checkboxes);
-  checkboxes.forEach(function (checkbox) {
-    checkbox.addEventListener("change", function () {
-      const selectedCategories = getSelectedCategories();
-      filterInformation(selectedCategories);
-    });
-  });
-
-  function getSelectedCategories() {
-    const selectedCategories = [];
-    checkboxes.forEach(function (checkbox) {
-      if (checkbox.checked) {
-        selectedCategories.push(checkbox.value);
-      }
-    });
-    return selectedCategories;
-  }
-
-  function filterInformation(categories) {
-    console.log(categories);
-    let findObjects = [];
-    let filterObjects;
-    findObjects = data.filter((obj) => {
-      for (let i = 0; i < categories.length; i++) {
-        console.log(categories[i]);
-        // console.log(obj.categories);
-        return categories[i] === obj.categories;
-      }
-    });
-
-    console.log(findObjects);
-  }
 }
